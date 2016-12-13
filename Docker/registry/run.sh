8b5c22cd335075208022fda2ce5599bcb17c13dc
Uggla <uggla@free.fr>
2016-12-13 01:00:33 +0100

    Add SAN.

--- Docker/registry/run.sh
+++ Docker/registry/run.sh
@@ -2,7 +2,7 @@
 umask 277 && openssl genrsa 2048 > ca/ca.key
 umask 007 && openssl req -new -x509 -days 365 -subj "/C=FR/ST=/L=Grenoble/O=HPE/CN=ca" -key ca/ca.key > ca/ca.crt
 umask 002 && openssl genrsa 2048 > srv/repo.key
-umask 002 && openssl req -new -subj "/C=FR/ST=/L=Grenoble/O=HPE/CN=$PUBIP" -key srv/repo.key > srv/repo.csr
+umask 002 && openssl req -new -subj "/C=FR/ST=/L=Grenoble/O=HPE/CN=$PUBIP/subjectAltName=DNS.1=$PUBIP" -key srv/repo.key > srv/repo.csr
 umask 002 && openssl x509 -req -in srv/repo.csr -out srv/repo.crt -CA ca/ca.crt -CAkey ca/ca.key -CAcreateserial -CAserial ca/ca.srl
 cp /home/pki/ca/ca.crt /var/www/html
 chown pki:pki /var/www/html/ca.crt && chmod 644 /var/www/html/ca.crt
