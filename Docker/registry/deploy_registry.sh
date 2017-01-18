13cbe17f0c4fb73f4e035dcf85a4941cea3a71db
Bruno Cornec <bruno@victoria.frmug.org>
2017-01-18 17:33:57 +0100

    Improve docker-compose setup

--- Docker/registry/deploy_registry.sh
+++ Docker/registry/deploy_registry.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Checking tool deps
-which docker-compose || curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m) > ./docker-compose
-chmod 755 docker-compose
+which docker-compose 
+if [ $? -ne 0 ]; then
+	if [ !  -f ./docker-compose ]; then
+		curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m) > ./docker-compose
+		chmod 755 docker-compose
+	fi
+fi
+
 for tool in sed docker
 do
 	echo "Checking $tool"
